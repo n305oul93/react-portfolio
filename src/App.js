1540457c4783d9af22424b5e505d95b3ddd127ff
D Pringle <n305oul93@gmail.com>
2021-01-17 13:54:22 -0600

    Adds & styles the react-plugin particles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Particles from 'react-particles-js';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 
 function App() {
   return (
     <>
+      <Particles
+        className='particles-canvas'
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900
+              }
+            },
+            shape: {
+              type: 'circle',
+              stroke: {
+                width: 6,
+                color: '#f9ab00'
+              }
+            }
+          }
+        }}
+      />
       <Navbar />
       <Header />
     </>
