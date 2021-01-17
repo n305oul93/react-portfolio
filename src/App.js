7deebebb8a2da04df465422319d75553c05dbc12
D Pringle <n305oul93@gmail.com>
2021-01-17 12:48:25 -0600

    Creates Header
    * Adds background image to header
    * Adds & styles typed text effect
    * Styles header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
+import Header from './components/Header';
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <Header />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Typed from 'react-typed';
+
+const Header = () => {
+  return (
+    <div className='header-wrapper'>
+      <div className='main-info'>
+        <h1>web development and website promotions</h1>
+        <Typed
+          className='typed-text'
+          strings={[
+            'Web Design',
+            'Web Development',
+            'Facebook Ads SMM',
+            'Google Ads'
+          ]}
+          typeSpeed={40}
+          backSpeed={60}
+          loop
+        />
+        <a href='#' className='btn-main-offer'>
+          contact me
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
