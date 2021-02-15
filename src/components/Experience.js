dda6c8cb1d4c24b5e3ee0a6ed8853eb3a9567dec
D Pringle <n305oul93@gmail.com>
2021-02-15 08:10:11 -0600

    Creates & styles the experience component

---
+++ src/components/Experience.js
@@ -0,0 +1,94 @@
+import React from 'react';
+
+const Experience = () => {
+  return (
+    <div className='experience'>
+      <div className='d-flex justify-content-center my-5'>
+        <h1>experience</h1>
+      </div>
+      <div className='container experience-wrapper'>
+        <div className='timeline-block timeline-block-right'>
+          <div className='marker'></div>
+          <div className='timeline-content'>
+            <h3>2014-2015</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+              numquam adipisci, facere eos recusandae reprehenderit rerum itaque
+              at et deleniti iure, animi ipsum corporis soluta accusamus cum,
+              nulla mollitia quis exercitationem ab doloribus a? Magni officia
+              consequatur quam aperiam accusantium!
+            </p>
+          </div>
+        </div>
+        {/* - */}
+        <div className='timeline-block timeline-block-left'>
+          <div className='marker'></div>
+          <div className='timeline-content'>
+            <h3>2014-2015</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+              numquam adipisci, facere eos recusandae reprehenderit rerum itaque
+              at et deleniti iure, animi ipsum corporis soluta accusamus cum,
+              nulla mollitia quis exercitationem ab doloribus a? Magni officia
+              consequatur quam aperiam accusantium!
+            </p>
+          </div>
+        </div>
+        <div className='timeline-block timeline-block-right'>
+          <div className='marker'></div>
+          <div className='timeline-content'>
+            <h3>2014-2015</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+              numquam adipisci, facere eos recusandae reprehenderit rerum itaque
+              at et deleniti iure, animi ipsum corporis soluta accusamus cum,
+              nulla mollitia quis exercitationem ab doloribus a? Magni officia
+              consequatur quam aperiam accusantium!
+            </p>
+          </div>
+        </div>
+        <div className='timeline-block timeline-block-left'>
+          <div className='marker'></div>
+          <div className='timeline-content'>
+            <h3>2014-2015</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+              numquam adipisci, facere eos recusandae reprehenderit rerum itaque
+              at et deleniti iure, animi ipsum corporis soluta accusamus cum,
+              nulla mollitia quis exercitationem ab doloribus a? Magni officia
+              consequatur quam aperiam accusantium!
+            </p>
+          </div>
+        </div>
+        <div className='timeline-block timeline-block-right'>
+          <div className='marker'></div>
+          <div className='timeline-content'>
+            <h3>2014-2015</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+              numquam adipisci, facere eos recusandae reprehenderit rerum itaque
+              at et deleniti iure, animi ipsum corporis soluta accusamus cum,
+              nulla mollitia quis exercitationem ab doloribus a? Magni officia
+              consequatur quam aperiam accusantium!
+            </p>
+          </div>
+        </div>
+        <div className='timeline-block timeline-block-left'>
+          <div className='marker'></div>
+          <div className='timeline-content'>
+            <h3>2014-2015</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+              numquam adipisci, facere eos recusandae reprehenderit rerum itaque
+              at et deleniti iure, animi ipsum corporis soluta accusamus cum,
+              nulla mollitia quis exercitationem ab doloribus a? Magni officia
+              consequatur quam aperiam accusantium!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
