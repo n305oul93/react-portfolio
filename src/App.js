3229735a11cdfc53197e2a667aa45cbed4f4133b
D Pringle <n305oul93@gmail.com>
2021-02-18 10:10:04 -0600

    Implements Contact form with validation

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Services from './components/Services';
 import Experience from './components/Experience';
 import Portfolio from './components/Portfolio';
 import Testimonials from './components/Testimonials';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -40,6 +41,7 @@ function App() {
       <Experience />
       <Portfolio />
       <Testimonials />
+      <Contact />
     </>
   );
 }
